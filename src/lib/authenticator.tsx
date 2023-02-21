07d76bcf4e5f492335772d68429abc275b31ca2f
Tom Woodley <thwoodley@gmail.com>
2023-02-21 21:44:44 +0000

    refactored login authenticator.tsx

---
+++ src/lib/authenticator.tsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+
+import { useToast } from './toast.context'
+
+export default function useAuth() {
+    const { addToast } = useToast()
+    const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+    const loginUser = (username: string, password: string) => {
+        // Here we can handle what students are trying to do
+        console.log("Looking up username: " + username + " and password: " + password + " in the database.")
+
+        let query = "SELECT * FROM Users WHERE UserId = '" + username + "' AND PassWord = '" + password + "' ;"
+
+        if (query.includes("--")) {
+            query = query.slice(0, query.indexOf("--"))
+        }
+        console.log(query)
+
+        let lowerUN = username.toLowerCase()
+
+
+
+        if (lowerUN.includes("'") && lowerUN.includes("--") && lowerUN.includes("or") && lowerUN.includes("=")) {
+            let eqIndex = lowerUN.indexOf("=")
+            let startIndex = lowerUN.indexOf("or") + 2
+            let endIndex = lowerUN.indexOf(" ", eqIndex) === -1 ? lowerUN.length : lowerUN.indexOf(" ", eqIndex);
+
+            let str1 = username.slice(startIndex, eqIndex).trim()
+            let str2 = username.slice(eqIndex + 1, endIndex).trim()
+
+            if (str1 === str2) {
+                setIsLoggedIn(true)
+            }
+        }
+
+        setIsLoggedIn(false)
+        addToast({ variant: 'error', title: 'Incorrect Username or Password' })
+    }
+
+    return { loginUser, isLoggedIn }
+}
+
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,48 +2,25 @@ import { useContext, useState } from 'react'
 import { Helmet } from 'react-helmet-async'
 
 import titles from '../constants/titles'
-import { useToast } from '../lib/toast.context'
 import { ThemeContext } from '../lib/theme.context'
 import { Container } from '../styles/_app.style'
 import { ActionButton } from '../styles/dialog.style'
 import { Fieldset, Form, Input, Label, Logo, Name, Tagline } from '../styles/login.style'
 import { SCHOOL_NAME } from '../constants/titles'
+import useAuth from "../lib/authenticator";
 
 /* TODO: Add a help toggle to the login form (i.e. information for new users to the platform) */
 const Login = () => {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
-  const { addToast } = useToast()
+  const { loginUser } = useAuth()
 
   const { theme } = useContext(ThemeContext)
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault()
 
-    // Here we can handle what students are trying to do
-    console.log("Trying username: " + username + " password: " + password)
-    let query = "SELECT * FROM Users WHERE UserId = '" + username + "' AND PassWord = '" + password + "' ;"
-    if (query.includes("--")) {
-      query = query.slice(0, query.indexOf("--"))
-    }
-    console.log(query)
-
-    let lowerUN = username.toLowerCase()
-
-    if (lowerUN.includes("'") && lowerUN.includes("--") && lowerUN.includes("or") && lowerUN.includes("=")) {
-      let eqIndex = lowerUN.indexOf("=")
-      let startIndex = lowerUN.indexOf("or") + 2
-      let endIndex = lowerUN.indexOf(" ", eqIndex) === -1 ? lowerUN.length : lowerUN.indexOf(" ", eqIndex);
-
-      let str1 = username.slice(startIndex, eqIndex).trim()
-      let str2 = username.slice(eqIndex + 1, endIndex).trim()
-
-      if (str1 === str2) {
-        return;
-      }
-    }
-
-    addToast({ variant: 'error', title: 'Incorrect Username or Password' })
+    loginUser(username, password)
   }
 
   return (
